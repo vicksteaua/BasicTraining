ca2c98d663b31cf8f5c1041aa6c59ba3878360d7
vikto <vikto@VICTOR-PC>
2020-03-19 15:25:45 +0200

    3

--- com.basic.training.main/AcceptString.java
+++ com.basic.training.main/AcceptString.java
@@ -1,6 +1,17 @@
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class AcceptString {
 	private String str;
@@ -33,6 +44,39 @@ public class AcceptString {
 			System.out.println(sc.nextLine());
 						
 		}
+	}
+	
+	public void acceptGUI() {
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container container= frame.getContentPane();
+		container.setLayout(new FlowLayout());
+		
+		final JTextField textfield = new JTextField();
+		textfield.setPreferredSize(new Dimension(150, 25));
+		
+		final JLabel label= new JLabel("Input string");
+		
+		JButton ok = new JButton("OK");
+		ok.addActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e) {
+				String input = textfield.getText();
+				System.out.println("Input");
+				
+				label.setText(input);
+				System.out.println("The string is "+ input);
+			}
+			
+		});
+		container.add(textfield);
+		container.add(label);
+		container.add(ok);
+		
+		frame.setVisible(true);
+		
 	}
 	
 
--- com.basic.training.main/Main.java
+++ com.basic.training.main/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		AcceptString a=new AcceptString("abc");
-		System.out.println(a.getStr());
-		a.acceptConsole();
-		a.acceptFile();
+		//System.out.println(a.getStr());
+		//a.acceptConsole();
+		//a.acceptFile();
+		a.acceptGUI();
 
 	}
 
