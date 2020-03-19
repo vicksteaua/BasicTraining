a4ccf62bb413b1023c1fe04c25fea4d9471f8583
vikto <vikto@VICTOR-PC>
2020-03-19 16:49:05 +0200

    123

---
+++ com.basic.training.main/StringOperations.java
@@ -0,0 +1,14 @@
+
+public class StringOperations {
+	
+	public int countStringChars(String str)
+	{
+		int count=0;
+		for(int i=0;i<str.length();i++ )
+		{
+			count++;
+		}
+		return count;
+	}
+		
+}
