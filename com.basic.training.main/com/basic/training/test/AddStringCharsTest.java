fe4d335b24ea466e305da074ecdb216743709e37
vikto <vikto@VICTOR-PC>
2020-03-27 19:57:38 +0200

    add string chars test

---
+++ com.basic.training.main/com/basic/training/test/AddStringCharsTest.java
@@ -0,0 +1,40 @@
+package com.basic.training.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.basic.training.StringOperations;
+
+public class AddStringCharsTest {
+	
+	@Test
+		public void addStringCharsNullTest() {
+		StringOperations stringOperations= new StringOperations();
+		assertThrows(NullPointerException.class, () -> stringOperations.addStringChars(null, "123", 0));
+		
+	}
+	
+	@Test
+	public void addStringCharsVoidTest() {
+	StringOperations stringOperations= new StringOperations();
+	assertEquals("abc",stringOperations.addStringChars("abc", "", 1));
+	
+}
+	
+	@Test
+	public void addStringCharsOneTest() {
+	StringOperations stringOperations= new StringOperations();
+	assertEquals("abcd",stringOperations.addStringChars("abc", "d", 3));
+	
+	}
+	
+	@Test
+	public void addStringCharsTwoTest() {
+	StringOperations stringOperations= new StringOperations();
+	assertEquals("abdec",stringOperations.addStringChars("abc", "de", 2));
+	
+	}
+
+}
